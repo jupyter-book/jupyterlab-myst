5b0ca9bea2f08207b4740b623023df567e60813b
Angus Hollands <goosey15@gmail.com>
2021-05-25 21:35:59 +0100

    Package: make jupyterlab-markup a requirement

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyter_server>=1.6,<2"
+        "jupyter_server>=1.6,<2",
+        "jupyterlab-markup~=1.0"
     ],
     zip_safe=False,
     include_package_data=True,
