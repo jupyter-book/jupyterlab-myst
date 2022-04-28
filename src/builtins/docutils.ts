cc347fd09e134d43af0cedded436f5c4c9e76c6a
Angus Hollands <goosey15@gmail.com>
2022-04-28 18:05:22 +0100

    WIP: support Math directive

--- src/builtins/docutils.ts
+++ src/builtins/docutils.ts
@@ -1,7 +1,31 @@
-import { simpleMarkdownItPlugin } from '@agoose77/jupyterlab-markup';
+import { simpleMarkdownItPlugin, IMarkdownIt} from '@agoose77/jupyterlab-markup';
 import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+import type MarkdownIt from 'markdown-it';
+import katex from 'katex';
 
 import { PACKAGE_NS } from '../tokens';
+
+
+// FIXME HACK: we should really do proper math rendering
+function wrapDocutilsPlugin(docutilsPlugin: IMarkdownIt.IPlugin)
+{
+    
+    return function (md: MarkdownIt, options: any)
+    {
+        docutilsPlugin(md, options);
+        
+        // Add renderer to MarkdownIt
+     md.renderer.rules["math_block"] = (tokens, idx) => {
+      const content = tokens[idx].content.trim()
+      const rendered = katex.renderToString(content, {
+        displayMode: true,
+        throwOnError: false,
+        output: "htmlAndMathml"
+      })
+      return `<div class="math">${rendered}</div>`
+    }
+    }
+}
 
 /**
  * Provides docutils roles and directives
@@ -23,7 +47,8 @@ export const docutils: JupyterFrontEndPlugin<void> = simpleMarkdownItPlugin(
       const docutilsPlugin = await import(
         /* webpackChunkName: "markdown-it-docutils" */ 'markdown-it-docutils'
       );
-      return [docutilsPlugin.default];
+      return [wrapDocutilsPlugin(docutilsPlugin.default)];
     }
   }
 );
+
