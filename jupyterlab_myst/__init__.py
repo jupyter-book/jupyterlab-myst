849b9a7afbfc258a462bc8763d15c4cdfcff4ca6
Angus Hollands <goosey15@gmail.com>
2022-06-10 22:28:20 +0100

    WIP: defer package.json parsing until required

--- jupyterlab_myst/__init__.py
+++ jupyterlab_myst/__init__.py
@@ -1,15 +1,13 @@
 import json
 from pathlib import Path
-from importlib.metadata import version
+from importlib_metadata import version
 
 __all__ = ["__version__"]
 __version__ = version(__name__)
 
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
 
 def _jupyter_labextension_paths():
+    here = Path(__file__).parent.resolve()
+    with (here / "labextension" / "package.json").open() as fid:
+        data = json.load(fid)
     return [{"src": "labextension", "dest": data["name"]}]
