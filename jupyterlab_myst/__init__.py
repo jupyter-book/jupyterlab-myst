fcac74b8032edc217ca2290d55aa776fc3552728
Angus Hollands <goosey15@gmail.com>
2022-06-10 22:24:58 +0100

    WIP: use importlib.metadata for version information

--- jupyterlab_myst/__init__.py
+++ jupyterlab_myst/__init__.py
@@ -1,15 +1,15 @@
 import json
 from pathlib import Path
+from importlib.metadata import version
 
-from ._version import __version__
+__all__ = ["__version__"]
+__version__ = version(__name__)
 
 HERE = Path(__file__).parent.resolve()
 
 with (HERE / "labextension" / "package.json").open() as fid:
     data = json.load(fid)
 
+
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": data["name"]
-    }]
+    return [{"src": "labextension", "dest": data["name"]}]
