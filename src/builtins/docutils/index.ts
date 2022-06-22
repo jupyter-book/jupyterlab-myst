161a97f36edfd519ea3af480fe57aad00e85922b
Angus Hollands <goosey15@gmail.com>
2022-06-22 21:54:37 +0100

    Feat: proper math (#52)
    * Package: bump version
    
    * Feat: pass math to DOM in $$ tags
    
    * Fix: remove KaTeX from style
    
    * Feat: add setup.py for JupyterLab develop
    
    * Package: bump markup dependency version
    
    * Refactor: drop math rendering from docutils
    
    We need to unify this across all math plugins, so for now just require the amsmath impl
    
    * Feat: add `sphinx-display-math` plugin
    
    This reproduces the sphinx treatment of display math for convenient multi-line and multiple-equation environments
    
    * Chore: uncap markup dependency

--- src/builtins/docutils/index.ts
+++ src/builtins/docutils/index.ts
@@ -1,7 +1,6 @@
 import { simpleMarkdownItPlugin } from '@agoose77/jupyterlab-markup';
 import { JupyterFrontEndPlugin } from '@jupyterlab/application';
 import type MarkdownIt from 'markdown-it';
-import katex from 'katex';
 
 import { PACKAGE_NS } from '../../tokens';
 import { EvalRole } from './roles';
@@ -9,33 +8,9 @@ import { EvalDirectiveAny, EvalFigureDirective } from './directives';
 
 import {
   directivesDefault,
-  rolesDefault,
-  IOptions
+  IOptions,
+  rolesDefault
 } from 'markdown-it-docutils';
-
-function splitPart(text: string): string {
-  if (text.includes('\\\\')) {
-    return `\\begin{split}${text}\\end{split}`;
-  } else {
-    return text;
-  }
-}
-
-function wrapDisplayMath(text: string): string {
-  const parts = text.split('\n\n');
-  const split_parts = parts.map(splitPart);
-
-  if (parts.length === 1) {
-    return `\\begin{equation}\\begin{split}${parts[0]}\\end{split}\\end{equation}\n`;
-  } else if (parts.length > 1) {
-    let result = ' \\begin{align}\\begin{aligned}';
-    result += split_parts.join('\\\\');
-    result += ' \\begin{aligned}\\end{align}';
-    return result;
-  } else {
-    return split_parts.join('////');
-  }
-}
 
 /**
  * Provides docutils roles and directives
@@ -74,29 +49,6 @@ export const docutils: JupyterFrontEndPlugin<void> = simpleMarkdownItPlugin(
           roles: roles,
           directives: directives
         });
-
-        // Add renderers to MarkdownIt
-        md.renderer.rules['math_block'] = (tokens, idx) => {
-          const token = tokens[idx];
-          const content = wrapDisplayMath(token.content.trim());
-          const rendered = katex.renderToString(content, {
-            displayMode: true,
-            throwOnError: false,
-            output: 'htmlAndMathml'
-          });
-          return `<div class="${token.attrGet('class')}">${rendered}</div>`;
-        };
-
-        md.renderer.rules['math_inline'] = (tokens, idx) => {
-          const token = tokens[idx];
-          const content = token.content.trim();
-          const rendered = katex.renderToString(content, {
-            displayMode: false,
-            throwOnError: false,
-            output: 'htmlAndMathml'
-          });
-          return `<span class="${token.attrGet('class')}">${rendered}</span>`;
-        };
       }
 
       return [wrappedDocutilsPlugin];
--- src/builtins/index.ts
+++ src/builtins/index.ts
@@ -2,8 +2,15 @@ import { frontMatter } from './front-matter';
 import { docutils } from './docutils';
 import { amsmath } from './amsmath';
 import { mystExtras } from './myst-extras';
+import { sphinxMath } from './sphinx-math';
 
 /**
  * Builtin plugins provided by this labextension
  */
-export const BUILTINS = [frontMatter, docutils, amsmath, mystExtras];
+export const BUILTINS = [
+  frontMatter,
+  docutils,
+  amsmath,
+  mystExtras,
+  sphinxMath
+];
