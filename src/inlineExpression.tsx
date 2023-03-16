4ae57e08b7ca70a7799a09acb49f9d8bd668ea1c
Rowan Cockett <rowanc1@gmail.com>
2023-03-16 11:33:58 -0600

    Update src/inlineExpression.tsx
    Co-authored-by: Angus Hollands <goosey15@gmail.com>

--- src/inlineExpression.tsx
+++ src/inlineExpression.tsx
@@ -171,7 +171,7 @@ export function InlineRenderer({ value }: { value?: string }): JSX.Element {
   }
 
   // Explicitly render text/plain
-  const preferred = rendermime.preferredMimeType(mimeBundle ?? {}, 'any');
+  const preferred = rendermime.preferredMimeType(mimeBundle ?? {}, trusted ? 'any' : 'ensure');
   if (preferred === 'text/plain') {
     return <PlainTextRenderer content={mimeBundle?.['text/plain'] as string} />;
   }
