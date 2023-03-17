586c8f781e901da3315da7e3840c4732841acefd
Angus Hollands <goosey15@gmail.com>
2023-03-17 07:25:43 +0000

    refactor: use `preferredMimeType` to identity text/plain

--- jupyterlab_myst/__init__.py
+++ jupyterlab_myst/__init__.py
@@ -1,4 +1,5 @@
 from ._version import __version__
+from .notary import MySTNotebookNotary
 
 
 def _jupyter_labextension_paths():
@@ -6,4 +7,18 @@ def _jupyter_labextension_paths():
         "src": "labextension",
         "dest": "jupyterlab-myst"
     }]
+
+
+def _load_jupyter_server_extension(app):  # pragma: no cover
+    contents = app.contents_manager
+    if not isinstance(contents.notary, MySTNotebookNotary):
+        contents.notary = MySTNotebookNotary(parent=contents)
+
+
+def _jupyter_server_extension_points():
+    """
+    Returns a list of dictionaries with metadata describing
+    where to find the `_load_jupyter_server_extension` function.
+    """
+    return [{"module": "jupyterlab_myst"}]
 
