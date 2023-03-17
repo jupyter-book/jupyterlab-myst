eea0746bcb13aef9c926df482c97c241956357f2
Angus Hollands <goosey15@gmail.com>
2023-03-17 07:28:33 +0000

    docs: add note about notary

--- jupyterlab_myst/__init__.py
+++ jupyterlab_myst/__init__.py
@@ -9,8 +9,11 @@ def _jupyter_labextension_paths():
     }]
 
 
-def _load_jupyter_server_extension(app):  # pragma: no cover
+def _load_jupyter_server_extension(app):
     contents = app.contents_manager
+
+    # Overwrite the default Notary with our own that understands Markdown trustedness
+    # This notary is less permissive (Markdown cells with untrusted expressions *also* invalidate the notebook trust)
     if not isinstance(contents.notary, MySTNotebookNotary):
         contents.notary = MySTNotebookNotary(parent=contents)
 
