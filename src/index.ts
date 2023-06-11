d9f4991e4d103f1aa74452f934e9a9024177a0de
Angus Hollands <goosey15@gmail.com>
2023-06-11 15:54:29 +0100

    chore: drop legacy mode

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,22 @@ import { IEditorServices } from '@jupyterlab/codeeditor';
 
 import {
   INotebookTracker,
-  INotebookWidgetFactory,
-  NotebookPanel,
-  NotebookWidgetFactory,
+  Notebook,
   NotebookActions,
-  Notebook
+  NotebookPanel
 } from '@jupyterlab/notebook';
 import { Cell } from '@jupyterlab/cells';
 import { MySTContentFactory } from './MySTContentFactory';
-
-import { ISessionContextDialogs } from '@jupyterlab/apputils';
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 
-import { ITranslator } from '@jupyterlab/translation';
-
 import { notebookCellExecuted } from './actions';
-import { mystIcon } from './icon';
 import { mystMarkdownRendererFactory } from './mime';
 
 /**
  * The notebook content factory provider.
  */
 const plugin: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
-  id: 'jupyterlab-myst:plugin',
+  id: 'jupyterlab-myst:content-factory',
   provides: NotebookPanel.IContentFactory,
   requires: [IEditorServices],
   autoStart: true,
@@ -38,84 +31,6 @@ const plugin: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
     console.log('JupyterLab extension jupyterlab-myst is activated!');
     const editorFactory = editorServices.factoryService.newInlineEditor;
     return new MySTContentFactory({ editorFactory });
-  }
-};
-
-/**
- * The legacy-mode content factory.
- */
-const legacyPlugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-myst:legacyPlugin',
-  optional: [ITranslator],
-  requires: [
-    IEditorServices,
-    IRenderMimeRegistry,
-    ISessionContextDialogs,
-    INotebookWidgetFactory,
-    INotebookTracker,
-    NotebookPanel.IContentFactory
-  ],
-  autoStart: true,
-  activate: (
-    app: JupyterFrontEnd,
-    editorServices: IEditorServices,
-    rendermime: IRenderMimeRegistry,
-    sessionContextDialogs: ISessionContextDialogs,
-    notebookFactory: NotebookWidgetFactory.IFactory,
-    notebookTracker: INotebookTracker,
-    existingContentFactory: NotebookPanel.IContentFactory,
-    translator: ITranslator | null
-  ) => {
-    if (existingContentFactory instanceof MySTContentFactory) {
-      return;
-    }
-    console.log(
-      'JupyterLab extension jupyterlab-myst (legacy mode) is activated!'
-    );
-
-    const contentFactory = new MySTContentFactory({
-      editorFactory: options =>
-        editorServices.factoryService.newInlineEditor(options)
-    });
-
-    const factory = new NotebookWidgetFactory({
-      name: 'Jupyter MyST Notebook',
-      // label: trans.__("Jupyter MyST Notebook"), // will be needed in JupyterLab 4
-      fileTypes: ['notebook', 'markdown', 'myst'],
-      defaultFor: ['notebook'],
-      modelName: notebookFactory.modelName ?? 'notebook',
-      preferKernel: notebookFactory.preferKernel ?? true,
-      canStartKernel: notebookFactory.canStartKernel ?? true,
-      rendermime,
-      contentFactory,
-      editorConfig: notebookFactory.editorConfig,
-      notebookConfig: notebookFactory.notebookConfig,
-      mimeTypeService: editorServices.mimeTypeService,
-      toolbarFactory: notebookFactory.toolbarFactory,
-      translator: translator ?? undefined
-    });
-
-    let id = 0;
-
-    factory.widgetCreated.connect((sender, widget) => {
-      // If the notebook panel does not have an ID, assign it one.
-      widget.id = widget.id || `myst-notebook-${++id}`;
-
-      // Set up the title icon
-      widget.title.icon = mystIcon ?? '';
-      widget.toolbar.title.icon = mystIcon;
-      widget.title.iconClass = '';
-      widget.title.iconLabel = 'MyST Notebook';
-
-      // Notify the widget tracker if restore data needs to update.
-      widget.context.pathChanged.connect(() => {
-        void (notebookTracker as any).save(widget);
-      });
-      // Add the notebook panel to the tracker.
-      void (notebookTracker as any).add(widget);
-    });
-
-    app.docRegistry.addWidgetFactory(factory);
   }
 };
 
@@ -141,7 +56,7 @@ const executorPlugin: JupyterFrontEndPlugin<void> = {
 };
 
 const mimeRendererPlugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-myst:mimeRenderer',
+  id: 'jupyterlab-myst:mime-renderer',
   requires: [IRenderMimeRegistry],
   autoStart: true,
   optional: [IMarkdownViewerTracker],
@@ -150,10 +65,10 @@ const mimeRendererPlugin: JupyterFrontEndPlugin<void> = {
     registry: IRenderMimeRegistry,
     tracker?: IMarkdownViewerTracker
   ) => {
-    console.log('Using jupyterlab-myst:mimeRenderer');
+    console.log('Using jupyterlab-myst:mime-renderer');
     // Add the MyST markdown renderer factory.
     registry.addFactory(mystMarkdownRendererFactory);
   }
 };
 
-export default [plugin, legacyPlugin, executorPlugin, mimeRendererPlugin];
+export default [plugin, executorPlugin, mimeRendererPlugin];
