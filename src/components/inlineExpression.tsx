dc12fc85921549c86a8310351e67e8b947412c88
Angus Hollands <goosey15@gmail.com>
2025-12-09 11:16:14 +0000

    chore: run prettier

--- src/components/inlineExpression.tsx
+++ src/components/inlineExpression.tsx
@@ -1,4 +1,4 @@
-import  { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useUserExpressions } from '../providers';
 import { SingletonLayout, Widget } from '@lumino/widgets';
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
